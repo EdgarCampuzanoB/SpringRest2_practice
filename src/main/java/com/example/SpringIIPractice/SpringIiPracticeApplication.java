2a65c448a7a41745a26468a1e16abc558909ab7a
EdgarCampuzanoB <passwordgithub.1226>
2021-11-16 09:48:26 -0600

    inicio practica

---
+++ src/main/java/com/example/SpringIIPractice/SpringIiPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringIIPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIiPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIiPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringIIPractice/SpringIiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringIIPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIiPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
